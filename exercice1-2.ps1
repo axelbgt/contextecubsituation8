d4d0eab1cdd9c19513320d62ec520afc16f569c9
axelbgt <axelbgtsio@gmail.com>
2023-02-28 10:33:20 +0100

    exercice1-2.ps1

--- exercice1-2.ps1
+++ exercice1-2.ps1
@@ -1,1 +1,9 @@
-﻿test
+﻿# Commentaire : Ce script permet de lister les numéros d'identifiants et les noms des processus svchost.
+
+# Obtenir tous les processus svchost en cours d'exécution
+$processes = Get-Process svchost
+
+# Boucle sur chaque processus svchost pour afficher son ID et son nom
+foreach ($process in $processes) {
+    Write-Output " $($process.Id) $($process.ProcessName)"
+}
