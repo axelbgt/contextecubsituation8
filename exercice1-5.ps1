ce13b5eb7826e4920a4bc77a79072960ee1880ca
axelbgt <axelbgtsio@gmail.com>
2023-02-28 11:18:04 +0100

    exercice1-5.ps1

--- exercice1-5.ps1
+++ exercice1-5.ps1
@@ -1,3 +1,10 @@
-﻿function InfoProcessus($processName) {
-    Get-Process | Where-Object {$_.ProcessName -eq $processName} | Select-Object Id, ProcessName, Description | Format-Table -AutoSize
+﻿# Définit la fonction InfoProcessus
+function InfoProcessus($processName)
+{
+    # Affiche les processus correspondants et leurs informations
+    Get-Process | Where-Object {$_.ProcessName -match $processName} | Select-Object Id,ProcessName,Description | Format-Table -AutoSize
 }
+
+
+# Appelle la fonction InfoProcessus avec le nom de processus en paramètre
+InfoProcessus "power"
