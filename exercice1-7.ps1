b560d04e3bbdb13b378e2b8587edb5a0637e9f43
axelbgt <axelbgtsio@gmail.com>
2023-02-28 11:48:31 +0100

    exercice1-7.ps1

---
+++ exercice1-7.ps1
@@ -0,0 +1,8 @@
+﻿# Obtenir la liste des services démarrés du système
+$services = Get-Service | Where-Object {$_.Status -eq "Running"} | Select-Object Name,Description,Status
+
+# Exporter les résultats dans un fichier CSV
+$services | Export-Csv -Path "services.csv" -Encoding utf8 -NoTypeInformation
+
+# Afficher les résultats dans la console
+$services | Format-Table -AutoSize
